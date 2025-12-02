3578d3ddbcd13c82b316c2ca22f344bcb4039801
Matt Carbonaro <mcarbona@voidheart.org>
2025-12-02 00:45:24 -0500

    day 2 go

--- 2025/day2/main.go
+++ 2025/day2/main.go
@@ -3,16 +3,87 @@ package main
 import (
 	"fmt"
 	"os"
+	"strconv"
+	"strings"
 
 	"github.com/akamensky/argparse"
 )
 
+func checkInvalidPart1(num int) bool {
+	str := strconv.Itoa(num)
+
+	if len(str)%2 == 1 {
+		return false
+	}
+
+	if str[:len(str)/2] != str[len(str)/2:] {
+		return false
+	}
+
+	return true
+}
+
+func checkInvalidPart2(num int) bool {
+	str := strconv.Itoa(num)
+
+newSize:
+	for size := 1; size <= len(str)/2; size += 1 {
+		if len(str)%size != 0 {
+			continue
+		}
+
+		check := str[0:size]
+
+		for i := size; i < len(str) && i+size <= len(str); i += size {
+			if str[i:i+size] != check {
+				continue newSize
+			}
+		}
+		return true
+	}
+
+	return false
+
+}
+
 func part1(input string) {
-	fmt.Printf("read file content:\n%s\n", input)
+	total := 0
+
+	for numRange := range strings.SplitSeq(input, ",") {
+		strRange := strings.Split(numRange, "-")
+		start, _ := strconv.Atoi(strRange[0])
+		end, _ := strconv.Atoi(strRange[1])
+
+		end += 1
+
+		for i := start; i <= end; i += 1 {
+			if checkInvalidPart1(i) {
+				total += i
+			}
+		}
+	}
+
+	fmt.Printf("Invalid numbers total: %d\n", total)
 }
 
 func part2(input string) {
-	fmt.Printf("read file content:\n%s\n", input)
+	total := 0
+
+	for numRange := range strings.SplitSeq(input, ",") {
+		strRange := strings.Split(numRange, "-")
+		start, _ := strconv.Atoi(strRange[0])
+		end, _ := strconv.Atoi(strRange[1])
+
+		end += 1
+
+		for i := start; i <= end; i += 1 {
+			if checkInvalidPart2(i) {
+				total += i
+			}
+		}
+	}
+
+	fmt.Printf("Invalid numbers total: %d\n", total)
 }
 
 func handle_cases(pt1 bool, pt2 bool, files []string) {
